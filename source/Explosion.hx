d052f3255703bfdbb8258001a549740d3b8dc65e
ooz <oliverzscheyge@gmail.com>
2015-01-24 16:36:50 +0100

    Update explosion

--- source/Explosion.hx
+++ source/Explosion.hx
@@ -8,8 +8,8 @@ class Explosion extends FlxSprite
     public function new()
     {
         super();
-        loadGraphic("assets/images/explosion3.png", true, 125, 128);
-        animation.add("explode", [0, 1, 2, 3, 4], 10, false);
+        loadGraphic("assets/images/explosion4.png", true, 125, 128);
+        animation.add("explode", [0, 1, 2, 3, 4, 5, 6], 12, false);
     }
 
     override public function update():Void
