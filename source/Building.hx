badbc7218dd8efd2ce5b04700db365fd99387abd
ooz <oliverzscheyge@gmail.com>
2015-01-24 15:06:03 +0100

    Add support for 2nd building type

--- source/Building.hx
+++ source/Building.hx
@@ -2,13 +2,19 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.util.FlxRandom;
 
 class Building extends FlxSprite
 {
     public function new(x:Float, y:Float)
     {
         super(x, y);
-        loadGraphic("assets/images/haus1_new.png", true, 105, 272);
+        switch (FlxRandom.intRanged(1, 2)) {
+            case 1:
+                loadGraphic("assets/images/haus1_new.png", true, 105, 272);
+            case 2:
+                loadGraphic("assets/images/haus2_new.png", true, 105, 244);
+        }
         animation.add("destroyed", [1], 1, true);
     }
 
