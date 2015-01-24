1f799dfe59ae7d490e8f138bb372e14e139b503c
ooz <oliverzscheyge@gmail.com>
2015-01-24 03:15:35 +0100

    Refactor some constants

--- source/GameMap.hx
+++ source/GameMap.hx
@@ -11,32 +11,36 @@ class GameMap extends FlxTilemap
     public var _height:Int = 20;
     public var _tileSize:Int = 128;
 
+    public static inline var TILE_TRANSPARENT = 0;
+    public static inline var TILE_BLOCK = 1;
+    public static inline var TILE_STREET_VERT = 2;
+    public static inline var TILE_STREET_HORI = 3;
+    public static inline var TILE_STREET_CROSS = 4;
+
     public function new()
     {
         super();
         var mapArray = [];
         for (i in 0..._width) {
             for (j in 0..._height) {
-                mapArray.push(1);
+                mapArray.push(GameMap.TILE_BLOCK);
             }
         }
         widthInTiles = _width;
         heightInTiles = _height;
         loadMap(mapArray, "assets/images/bodentextur.png", _tileSize, _tileSize);
 
-        
-//        for (i in 0..._width) {
-//            for (j in 0..._height) {
-//                setTile(i, j, );
-//            }
-//        }
         // von Silvio
         var strassen_x:Int = Std.int(_width / 10);
         for (sx in 0...strassen_x) {
             var sp = FlxRandom.intRanged(0, _width - 1);
-            if (sp > 1 && sp < _height - 2 && getTile(sp-1, 0) != 2 && getTile(sp-2, 0) != 2 && getTile(sp+1, 0) != 2 && getTile(sp+2, 0) != 2) {
+            if (sp > 1 && sp < _height - 2 
+                && getTile(sp-1, 0) != GameMap.TILE_STREET_VERT
+                && getTile(sp-2, 0) != GameMap.TILE_STREET_VERT 
+                && getTile(sp+1, 0) != GameMap.TILE_STREET_VERT 
+                && getTile(sp+2, 0) != GameMap.TILE_STREET_VERT) {
                 for (c in 0..._width) {
-                    setTile(sp, c, 2);
+                    setTile(sp, c, GameMap.TILE_STREET_VERT);
                 }
             }
         }
@@ -44,15 +48,20 @@ class GameMap extends FlxTilemap
         var strassen_y:Int = Std.int(_height / 10);
         for (sy in 0...strassen_y) {
             var sp = FlxRandom.intRanged(0, _height - 1);
-            if (sp > 1 && sp < _width-2 && getTile(0,sp-1) != 3 && getTile(0,sp-2) != 3 && getTile(0, sp+1) != 3 && getTile(0, sp+2) != 3) {
+            if (sp > 1 && sp < _width-2 
+                && getTile(0,sp-1) !=  GameMap.TILE_STREET_HORI 
+                && getTile(0,sp-2) !=  GameMap.TILE_STREET_HORI 
+                && getTile(0, sp+1) != GameMap.TILE_STREET_HORI 
+                && getTile(0, sp+2) != GameMap.TILE_STREET_HORI) {
                 for (c in 0..._height) {
-                    if (getTile(c, sp) == 2 || getTile(c, sp) == 4)
+                    if (getTile(c, sp) == GameMap.TILE_STREET_VERT
+                        || getTile(c, sp) == GameMap.TILE_STREET_CROSS)
                     {
-                        setTile(c, sp, 4);
+                        setTile(c, sp, GameMap.TILE_STREET_CROSS);
                     }
                     else
                     {
-                        setTile(c, sp, 3);
+                        setTile(c, sp, GameMap.TILE_STREET_HORI);
                     }
                 }
             }
