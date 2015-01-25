b06d7f87859aea6100245efa52a68f199ce0e74b
ooz <oliverzscheyge@gmail.com>
2015-01-25 15:33:06 +0100

    Add credits

--- source/AboutState.hx
+++ source/AboutState.hx
@@ -1,17 +1,24 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxG;
 import flixel.ui.FlxButton;
+using flixel.util.FlxSpriteUtil;
 
 class AboutState extends FlxState
 {
+    private var _creditsImg:FlxSprite;
     private var _btnCredits:FlxButton;
 
     override public function create():Void
     {
         FlxG.mouse.visible = true;
         bgColor = 0xFFFFFFFF;
+
+        _creditsImg = new FlxSprite(0, 0, "assets/images/credits.png");
+        _creditsImg.screenCenter();
+        add(_creditsImg);
 
         _btnCredits = new FlxButton(1024 - 128, 576 - 32, "", clickBack);
         _btnCredits.loadGraphic("assets/images/backbutton_small.png");
